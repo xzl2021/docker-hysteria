36773ee794d1bcc24ce61d63afed95887fb832c4
Lyraa <xzl2021@hotmail.com>
2024-10-16 15:02:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /etc/hysteria/
 cp ./server.yaml /etc/hysteria/
-cp ./cert.crt /etc/hysteria/
-cp ./private.key /etc/hysteria/
-docker run -d -p 0.0.0.0:8443:8443/udp -v /etc/hysteria:/etc/hysteria --restart=always --name hy2 hysteria2:latest
+cp ./ca.crt /etc/hysteria/
+cp ./ca.key /etc/hysteria/
+docker run -d -p 0.0.0.0:8443:8443/udp -v /etc/hysteria:/etc/hysteria --restart=always --name hy hysteria:latest
